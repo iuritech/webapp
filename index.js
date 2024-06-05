e890fa2f620e1c52f2263f99530150490ccf9860
iuritech <iurisn@live.com.pt>
2024-06-05 18:08:03 +0100

    aula index

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-// index.js
-const express = require('express');
-const path = require('path');
+// // index.js
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
-const port = 8080;
+// const app = express();
+// const port = 8080;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// // Serve static files from the "public" directory
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+
+var express = require('express')
+
+var app = express()
+const port = process.env.PORT || 8080
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(port, function () {
+  console.log('Example app listening on port ${port}!');
 });
 
