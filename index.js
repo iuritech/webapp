ff64f5a28efcda0f8394883a5d951695717cea5a
iuritech <iurisn@live.com.pt>
2024-06-06 10:10:02 +0100

    'teste'

--- index.js
+++ index.js
@@ -9,32 +9,32 @@ const CosmosClient = require('@azure/cosmos').CosmosClient
     const key = process.env.COSMOS_KEY;
     const endpoint = process.env.COSMOS_ENDPOINT;
 
-//    console.log(`Using the endpoint: ${endpoint}`);
+   console.log(`Using the endpoint: ${endpoint}`);
 
     // Authenticate to Azure Cosmos DB
     const cosmosClient = new CosmosClient({ endpoint, key });
 
-    // // Get the database object
-    // const db=cosmosClient.database('tonytectosDB');
+    // Get the database object
+    const db=cosmosClient.database('tonytectosDB');
 
-    // // Get the container object
-    // const container=db.container('tonytectosContainer');
+    // Get the container object
+    const container=db.container('tonytectosContainer');
 
-    // // preparing the query
-    // const querySpec = {
-    //     query: 'SELECT * FROM items'
-    // };
+    // preparing the query
+    const querySpec = {
+        query: 'SELECT * FROM items'
+    };
 
-    // // Get items
-    // const { resources } = await container.items.query(querySpec).fetchAll();
+    // Get items
+    const { resources } = await container.items.query(querySpec).fetchAll();
 
-    // // Print headings
-    // console.log(`\nId \t Nome`);
+    // Print headings
+    console.log(`\nId \t Nome`);
 
-    // // show the results
-    // for (const item of resources) {
-    //     console.log(`${item.id} \t ${item.artigos}`);
-    // }
+    // show the results
+    for (const item of resources) {
+        console.log(`${item.id} \t ${item.artigos}`);
+    }
 
 //termina filtros
 
